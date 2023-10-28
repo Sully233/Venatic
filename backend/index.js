efd36ff764dca29fcdcb0925926f2a2da4cf53fd
PatchingInitiative <126059316+PatchingInitiative@users.noreply.github.com>
2023-10-29 00:15:34 +1100

    backend ci/cd pipeline
    created ci/cd pipeline for backend

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,26 @@
-const express = require('express')
+import express from "express";
+import helmet from "helmet";
+import serverless from "serverless-http";
+
 const app = express();
 const PORT = 8080;
 
-app.use(express.json())
+app.use(helmet());
+app.use(express.json());
 
 app.get('/tshirt', (req, res) => {
-
     res.status(200).send({
-        tshirt: 'large'
-    })
-
+        tshirt: 'LARGE'
+    });
 });
-
 
 app.post('/tshirt/:id', (req, res) => {
-
     const { id } = req.params;
     const { logo } = req.body;
-
     res.json(`${id} and ${logo}`);
-
-
 });
 
-app.listen(
-    PORT,
-    () => console.log('alive')
-)
+// app.listen(PORT, () => console.log('Server is alive on port ' + PORT));
 
+
+export const handler = serverless(app)
