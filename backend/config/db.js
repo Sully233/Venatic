920d2c39bc791efb9065848bca3d2671e7f1befb
PatchingInitiative <126059316+PatchingInitiative@users.noreply.github.com>
2023-10-30 23:59:50 +1100

    db bug fix
    bug fix db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: process.env.DB_NAME
+        });
+
 
         console.log(`Mongo Connected: ${conn.connection.host}`)
     } catch (error) { 
