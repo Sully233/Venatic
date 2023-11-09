65d89c9e113c1806712e440d7ea6dac755948d1a
PatchingInitiative <126059316+PatchingInitiative@users.noreply.github.com>
2023-11-10 10:13:45 +1100

    test ?

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <h1 className="text-4xl font-bold leading-tight text-gray-900">
         Hypothetical Landing Page
       </h1>
-      <div>Blah blah blah cool website!!!!!</div>
+      <div>Blah blah blah cool website!</div>
       <div>Lorem ipsum dolor would be nice if we knew what subsidiary we're doing!</div>
       <Link to="/booking">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-3">
