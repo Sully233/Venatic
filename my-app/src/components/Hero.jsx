931dcd52df7d8cf8ec011edb9e7261f757731031
Yuhan Dong <60382478+YuhanDong@users.noreply.github.com>
2023-11-09 17:15:32 +1100

    First commit for Hero component

---
+++ my-app/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div>blah blah blah cool website!</div>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Button
+      </button>
+    </div>
+  );
+};
+
+export default Hero;
