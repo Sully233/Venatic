6a6bd79a42ebdca86130334484ce963c230986db
Yuhan Dong <60382478+YuhanDong@users.noreply.github.com>
2023-11-09 17:28:43 +1100

    First commit for booking form page

---
+++ my-app/src/components/Booking.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Booking = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold leading-tight text-gray-900">
+        Booking Page
+      </h1>
+    </div>
+  );
+};
+
+export default Booking;
