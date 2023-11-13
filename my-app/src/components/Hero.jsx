0eb51c9237b6702884e72dfa54b930ec30393018
Yuhan Dong <60382478+YuhanDong@users.noreply.github.com>
2023-11-13 16:49:40 +1100

    add picture to front page

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="w-full max-w-4xl">
-        <div className="ml-1/3">
-          <h1 className="text-5xl font-bold leading-tight text-gray-900">
-            Perfect Portraits 
+      <div className="w-full max-w-4xl grid grid-cols-2 gap-20">
+        <div className="first-quarter flex flex-col justify-center">
+          <h1 className="text-5xl font-bold leading-tight text-gray-900 whitespace-nowrap">
+            Perfect Portraits
           </h1>
           <h1 className="text-5xl font-bold leading-tight text-gray-900">
             for every
@@ -15,13 +15,16 @@ const Hero = () => {
           <h1 className="text-5xl font-bold leading-tight text-gray-900">
             season of life.
           </h1>
-          <div className="mt-8 text-xl">Feel the Magic of Each Moment, Forever.</div>
+          <div className="mt-8 text-xl">
+            Feel the Magic of Each Moment, Forever.
+          </div>
           <Link to="/booking">
             <button className="mt-8 bg-orange-500 hover:bg-orange-600 text-white font-bold py-3 px-8 rounded-full text-sm">
               BOOK A SESSION
             </button>
           </Link>
         </div>
+        <div class="picture1">&nbsp;</div>
       </div>
     </div>
   );
