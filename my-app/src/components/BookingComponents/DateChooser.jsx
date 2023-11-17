4360fbb17a9282472d4905060b1b5bd05e651103
Yuhan Dong <60382478+YuhanDong@users.noreply.github.com>
2023-11-17 18:48:45 +1100

    update datechooser to work with hook form

--- my-app/src/components/BookingComponents/DateChooser.jsx
+++ my-app/src/components/BookingComponents/DateChooser.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import TextField from '@mui/material/TextField';
 
-export default function DatePickerMaterialUI() {
+export default function DatePickerMaterialUI({ selectedDate, handleDateChange }) {
   return (
     <div>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker />
+        <DatePicker
+          value={selectedDate}
+          onChange={handleDateChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
       </LocalizationProvider>
     </div>
   );
