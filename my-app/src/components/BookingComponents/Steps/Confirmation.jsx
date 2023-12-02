78e9837f95a371dd400b76f335b3f0f75416b80a
PatchingInitiative <126059316+PatchingInitiative@users.noreply.github.com>
2023-12-02 17:08:18 +1100

    Update Confirmation.jsx

--- my-app/src/components/BookingComponents/Steps/Confirmation.jsx
+++ my-app/src/components/BookingComponents/Steps/Confirmation.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { observer } from "mobx-react";
 import { formStore } from "../../../stores/FormStore";
 import { motion, AnimatePresence } from "framer-motion";
+import { customerDetailsStore } from "../../../stores/CustomerDetailsStore";
 
 import "../form.css";
 
@@ -10,16 +11,16 @@ const Confirmation = observer((allFields) => {
     <div className="space-y-2">
       <h3 className="text-lg font-semibold">Please review your details:</h3>
       <p>
-        <strong>First Name:</strong> {allFields.firstName}
+        <strong>First Name:</strong> {customerDetailsStore.firstName}
       </p>
       <p>
-        <strong>Last Name:</strong> {allFields.lastName}
+        <strong>Last Name:</strong> {customerDetailsStore.lastName}
       </p>
       <p>
-        <strong>Email:</strong> {allFields.email}
+        <strong>Email:</strong> {customerDetailsStore.email}
       </p>
       <p>
-        <strong>Phone:</strong> {allFields.phone}
+        <strong>Phone:</strong> {customerDetailsStore.phone}
       </p>
       <p>
         <strong>Address:</strong> {formStore.address}
