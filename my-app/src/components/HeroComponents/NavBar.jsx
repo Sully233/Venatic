0e8351839347bc3ca27bb1c13af27e8523a244bd
PatchingInitiative <126059316+PatchingInitiative@users.noreply.github.com>
2023-12-16 12:26:48 +1100

    bug fix

--- my-app/src/components/HeroComponents/NavBar.jsx
+++ my-app/src/components/HeroComponents/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="flex flex-col md:flex-row justify-between items-center p-4 bg-white shadow-md">
+    <nav className="flex flex-col md:flex-row justify-between items-center p-4 bg-white ">
       <div className="flex justify-between items-center w-full md:w-auto">
         <div className="logo flex items-center">
           <img src={logoImage} alt="Venatic Logo" className="w-12 h-12 md:w-16 md:h-16" />
@@ -31,6 +31,3 @@ const NavBar = () => {
 
 export default NavBar;
 
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-</svg>
