af98fdfe4ed52f5745fcf392d933f393cabf03ac
PatchingInitiative <126059316+PatchingInitiative@users.noreply.github.com>
2023-12-16 11:52:18 +1100

    mobile optimised navbar

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { observer } from "mobx-react";
 import { motion } from "framer-motion";
@@ -6,54 +6,45 @@ import { formStore } from "../stores/FormStore";
 import MultiStepForm from "./BookingComponents/MultiStepForm";
 import ReactGA from "react-ga4";
 import { Helmet } from "react-helmet";
-import NavBar from './HeroComponents/NavBar';
-import Footer from './HeroComponents/Footer';
-
+import NavBar from "./HeroComponents/NavBar";
+import Footer from "./HeroComponents/Footer";
 
 const Hero = observer(() => {
-
-
-
   useEffect(() => {
-    ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
-
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
   }, []);
 
-    return (
-      
-      <div className="min-h-screen flex flex-col">
-      <NavBar></NavBar>
+  return (
+    <div className="min-h-screen flex flex-col">
+      <NavBar/>
       <div className="flex flex-1 justify-center items-center p-4">
         <Helmet>
           <title>Venatic</title>
-        </Helmet>  
+        </Helmet>
         <div className="w-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="flex flex-col justify-center text-center md:text-left">
-          <h1 className="text-3xl md:text-5xl font-bold leading-tight text-gray-900">
-            Perfect Portraits
-          </h1>
-          <h2 className="text-3xl md:text-5xl font-bold leading-tight text-gray-900">
-            for every
-          </h2>
-          <h3 className="text-3xl md:text-5xl font-bold leading-tight text-gray-900">
-            season of life.
-          </h3>
-          <div className="mt-4 md:mt-8 text-lg md:text-xl">
-            Feel the Magic of Each Moment, Forever.
+          <div className="flex flex-col justify-center text-center md:text-left">
+            <h1 className="text-3xl md:text-5xl font-bold leading-tight text-gray-900">
+              Perfect Portraits
+            </h1>
+            <h2 className="text-3xl md:text-5xl font-bold leading-tight text-gray-900">
+              for every
+            </h2>
+            <h3 className="text-3xl md:text-5xl font-bold leading-tight text-gray-900">
+              season of life.
+            </h3>
+            <div className="mt-4 md:mt-8 text-lg md:text-xl">
+              Feel the Magic of Each Moment, Forever.
+            </div>
+            <Link to="/booking" className="mt-4 md:mt-8"></Link>
           </div>
-          <Link to="/booking" className="mt-4 md:mt-8"></Link>
-
-        </div>
-        <div className="order-last md:order-last flex justify-center md:justify-end">
-          <div className="w-full md:min-w-[500px] md:w-1/2 bg-slate-50 rounded-xl">
-            <MultiStepForm />
+          <div className="order-last md:order-last flex justify-center md:justify-end">
+            <div className="w-full md:min-w-[500px] md:w-1/2 bg-slate-50 rounded-xl">
+              <MultiStepForm />
+            </div>
           </div>
         </div>
       </div>
-
-    </div>
-    <Footer></Footer>    
-
+      <Footer/>
     </div>
   );
 });
--- my-app/src/components/HeroComponents/NavBar.jsx
+++ my-app/src/components/HeroComponents/NavBar.jsx
@@ -1,22 +1,36 @@
-import React from "react"
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
-
+import logoImage from "../../images/logo0.png";
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'; // This is for Heroicons v2
 
 const NavBar = () => {
-    return (
-      <nav className="flex justify-between items-center p-4 bg-white shadow-md">
-        <div className="logo">
-          <Link to="/" className="text-xl font-bold">Venatic</Link>
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="flex flex-col md:flex-row justify-between items-center p-4 bg-white shadow-md">
+      <div className="flex justify-between items-center w-full md:w-auto">
+        <div className="logo flex items-center">
+          <img src={logoImage} alt="Venatic Logo" className="w-12 h-12 md:w-16 md:h-16" />
+          <Link to="/" className="text-xl font-bold px-1">Venatic</Link>
         </div>
-        <div className="nav-links flex gap-4">
+        <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <XMarkIcon className="h-6 w-6" /> : <Bars3Icon className="h-6 w-6" />}
+        </button>
+      </div>
+      <div className={`nav-links-container ${isOpen ? 'flex' : 'hidden'} md:flex mt-4 md:mt-0`}>
+        <div className="nav-links flex flex-col md:flex-row gap-4">
           <Link to="/about" className="hover:text-blue-500 transition duration-300">About</Link>
           <Link to="/services" className="hover:text-blue-500 transition duration-300">Services</Link>
           <Link to="/contact" className="hover:text-blue-500 transition duration-300">Contact</Link>
           <Link to="/booking" className="hover:text-blue-500 transition duration-300">Booking</Link>
         </div>
-      </nav>
-    );
-  };
+      </div>
+    </nav>
+  );
+};
 
-  export default NavBar;
+export default NavBar;
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
